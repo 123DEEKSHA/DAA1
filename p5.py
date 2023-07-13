dee1884c0a172a49208b2d64430b74ea92a2c2f7
123DEEKSHA <120497234+123DEEKSHA@users.noreply.github.com>
2023-07-13 07:11:23 +0000

    commit

--- p5.py
+++ p5.py
@@ -1,3 +1,4 @@
+
 import heapq
  
  
@@ -9,13 +10,13 @@ class node:
        
         self.symbol = symbol
  
-        
+       
         self.left = left
  
-        
+       
         self.right = right
  
-        
+       
         self.huff = ''
  
     def __lt__(self, nxt):
@@ -28,7 +29,7 @@ def printNodes(node, val=''):
     
     newVal = val + str(node.huff)
  
-   
+    
     if node.left :
         printNodes(node.left, newVal)
     if(node.right):
@@ -39,11 +40,11 @@ def printNodes(node, val=''):
         print(f"{node.symbol} -> {newVal}")
  
  
-
-chars = ['a', 'b', 'c', 'd', 'e', 'f']
+# characters for huffman tree
+chars = input("Enter the characters:").split(' ') #['a', 'b', 'c', 'd', 'e', 'f']
  
-
-freq = [5, 9, 12, 13, 16, 45]
+# frequency of characters
+freq =[int(i) for i in input("Enter the frequency:").split(' ')] #[5, 9, 12, 13, 16, 45]
  
 
 nodes = []
@@ -54,17 +55,61 @@ for x in range(len(chars)):
  
 while len(nodes) > 1:
  
+    
     left = heapq.heappop(nodes)
     right = heapq.heappop(nodes)
  
-  
+    
     left.huff = 0
     right.huff = 1
  
-                                                                                    
+    
     newNode = node(left.freq+right.freq, left.symbol+right.symbol, left, right)
  
     heapq.heappush(nodes, newNode)
  
-
+# Huffman Tree is ready!
 printNodes(nodes[0])
+def decode_text(root, encoded_text):
+    decoded_text = ""
+    current_node = root
+
+    for bit in encoded_text:
+        if bit == "0":
+            current_node = current_node.left
+        else:
+            current_node = current_node.right
+
+        if current_node.left is None and current_node.right is None:
+            decoded_text += current_node.symbol
+            current_node = root
+
+    return decoded_text
+encoded_text = (input("enter the code:"))
+decoded_text = decode_text(nodes[0], encoded_text)
+print("Decoded text:", decoded_text)
+
+
+def encode_text(root, text):
+    huffman_codes = {}
+
+    def generate_codes(node, current_code=""):
+        if node is None:
+            return
+
+        if node.symbol:
+            huffman_codes[node.symbol] = current_code
+
+        generate_codes(node.left, current_code + "0")
+        generate_codes(node.right, current_code + "1")
+
+    generate_codes(root)
+
+    encoded_text = ""
+    for char in text:
+        encoded_text += huffman_codes[char]
+
+    return encoded_text
+text=input("Enter text:")
+encoded_text = encode_text(nodes[0], text)
+print("Encoded text:", encoded_text)
